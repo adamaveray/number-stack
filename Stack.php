0fa3fb0925c0a66f8b943bb8f8673b6352775f25
Adam Averay <adam.averay@gmail.com>
2013-02-26 18:26:00 -0500

    Created Stack skeleton

---
+++ Stack.php
@@ -0,0 +1,12 @@
+<?php
+class Stack implements \Interfaces\Stack {
+	protected $top;
+
+	public function push(\Interfaces\Node $item){
+
+	}
+
+	public function pop(){
+
+	}
+};
--- index.php
+++ index.php
@@ -3,3 +3,4 @@ include('Interface/Node.php');
 include('Interface/Stack.php');
 include('Interface/NumberStack.php');
 include('Node.php');
+include('Stack.php');
