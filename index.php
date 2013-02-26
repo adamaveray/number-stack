7324cb69e99ece00a327fc32f70b82c9798b113a
Adam Averay <adam.averay@gmail.com>
2013-02-26 18:44:25 -0500

    Created Stack demo

--- index.php
+++ index.php
@@ -2,3 +2,22 @@
 spl_autoload_register(function($class){
 	require(str_replace('\\', '/', $class).'.php');
 });
+
+
+function demo_stack(){
+	$items	= array(10, 2, 5, 8, 1, 7);
+
+	$stack	= new \NumberStack();
+	echo 'Pushing'.PHP_EOL;
+	foreach($items as $item){
+		echo $item.' , ';
+		$stack->push(new \Node($item));
+	}
+
+	try {
+		echo 'Popping'.PHP_EOL;
+		while(true){
+			echo $stack->pop()->get_value().' , ';
+		}
+	} catch(\Exception $e){}
+}
