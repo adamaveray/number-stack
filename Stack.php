4ac0ca2818a228ae1f0782fd20355afea7d67e21
Adam Averay <adam.averay@gmail.com>
2013-02-26 18:32:55 -0500

    Set up `push` for Stack

--- Stack.php
+++ Stack.php
@@ -6,7 +6,11 @@ class Stack implements \Interfaces\Stack {
 	protected $top;
 
 	public function push(\Interfaces\Node $item){
+		if(isset($this->top)){
+			$item->set_next($this->top);
+		}
 
+		$this->top	= &$item;
 	}
 
 	public function pop(){
