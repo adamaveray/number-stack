35ebf6f1de075c048062213db96fc88843fc8823
Adam Averay <adam.averay@gmail.com>
2013-02-26 19:05:16 -0500

    Added NumberStack demo

--- index.php
+++ index.php
@@ -22,4 +22,34 @@ function demo_stack(){
 	} catch(\Exception $e){}
 }
 
+
+function demo_number_stack(){
+	$items	= array(5, 10, 2, 5, 12, 1, 13, 2);
+
+	$stack	= new \NumberStack();
+	echo 'Pushing'.PHP_EOL;
+	foreach($items as $item){
+		echo $item.' , ';
+		$stack->push(new \Node($item));
+	}
+
+	echo 'Highest: '.$stack->get_highest()->get_value().PHP_EOL;
+
+	try {
+		echo 'Popping'.PHP_EOL;
+		while(true){
+			echo $stack->pop()->get_value().' , ';
+		}
+	} catch(\Exception $e){}
+}
+
+?>
+<h2>Standard Stack</h2>
+<pre><?php
 demo_stack();
+?></pre>
+
+<h2>Number Stack</h2>
+<pre><?php
+demo_number_stack();
+?></pre>
